fbf24997080cb94b0421989b3a261fe658ed04a5
Yahya El Kenani <yahyamahmoud887@gmail.com>
2025-07-18 20:03:49 +0300

    fixing api

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
+const path = require("path");
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-    console.log('JSON Server is running');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
 });
